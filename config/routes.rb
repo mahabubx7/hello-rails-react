31b6e666c80d26db9ff869b9bed76c0525027a7b
Mahabub Alam <mahabubx7@gmail.com>
2023-08-23 00:30:36 +0600

    setup(greeting):hammer: added-greetings-init-code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "root#index"
+  root 'root#index'
 end
